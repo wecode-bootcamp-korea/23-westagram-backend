b5d2bf3eb92091209d0023443327075884306287
preferbaik <baik9261@gmail.com>
2021-07-22 18:48:38 +0900

    project model4

--- students/sunhobaik/users/models.py
+++ students/sunhobaik/users/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import TextField, IntegerField
+
 
 
 class User(models.Model):
-    name         =models.TextField(max_length=20)
-    email        =models.TextField(max_length=100)
-    password     =models.TextField(max_length=20)
-    phone_numbers=models.IntegerField()
-    privacy      =models.TextField(max_length=200)
-    
+    name          = models.CharField(max_length=20)
+    email         = models.TextField(max_length=100)
+    password      = models.TextField(max_length=20)
+    phone_numbers = models.CharField(max_length=20)
+    web_site      = models.TextField(max_length=30)
+    nick_name     = models.CharField(max_length=20) 
+    address       = models.TextField(max_length=100)
+    introduce     = models.TextField(max_length=200)
     def Meta(self):
         db_table = 'users'
 
