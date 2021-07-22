7d4e1ac12b5352d8acea42256bef56289dcb2caa
choihyerim <hyerimc858@gmail.com>
2021-07-22 20:50:31 +0900

    Modify: users models

--- students/hyerim/users/models.py
+++ students/hyerim/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    mobile = models.PositiveIntegerField()
+    mobile = models.CharField(max_length=100)
     other_info = models.CharField(max_length=1000)
 
     class Meta:
