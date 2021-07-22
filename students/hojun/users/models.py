d764a389cd19ee7828c3b213930f0c13764c220d
jun <wkdghwns91@gmail.com>
2021-07-22 19:43:56 +0900

    Modify : 클래스 명 User로 수정, 문법 오류 수정

--- students/hojun/users/models.py
+++ students/hojun/users/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class SignUp(models.Model):
-    name     = models.CharField(max_length=45)
-    email    = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    phone_number = models.PositiveIntegerField()
+class User(models.Model):
+    name          = models.CharField(max_length=45)
+    email         = models.CharField(max_length=100)
+    password      = models.CharField(max_length=100)
+    phone_number  = models.PositiveIntegerField()
     personal_info = models.TextField(max_length=500)
 
     class Meta:
-        db_table : 'signups'
+        db_table = "users"
