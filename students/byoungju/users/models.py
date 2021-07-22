7bcf53a218a4336f599411c7a9a5b953bbb4347a
byoungjupark <byoungju1012@gmail.com>
2021-07-22 21:08:56 +0900

    Update: models

--- students/byoungju/users/models.py
+++ students/byoungju/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45)
+    name     = models.CharField(max_length=45)
+    email    = models.CharField(max_length=100)
     password = models.CharField(max_length=45)
-    phone = models.PositiveIntegerField()
-    birth = models.DateField(null=True)
+    phone    = models.CharField(max_length=20)
+    birth    = models.DateField()
 
     class Meta:
         db_table = "users"
