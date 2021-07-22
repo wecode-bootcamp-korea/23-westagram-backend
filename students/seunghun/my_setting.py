620d3a049f39053c283874a2617dc49d203ede2f
Samdaso-o <soc05214@naver.com>
2021-07-22 17:58:10 +0900

    project_initial_setting

---
+++ students/seunghun/my_setting.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': 'hoon0327',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-&^rm3i#^48suko6#2af-y6fr27w$xx73a)1ff(&$bw^*79b@rz' 
