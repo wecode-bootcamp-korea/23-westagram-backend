2eeb102fd6b54072e7196adce01aa36a27e11523
lunayyko <youryoung.ko@gmail.com>
2021-07-22 19:33:39 +0900

    ADD: Mission2

---
+++ students/yooyoung/users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    note = models.TextField(max_lenth=100, null=True)
+    phone = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,15}$',
+                message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+            ),
+        ],
+    )
+    
+    class Meta:
+        db_table = 'Users'
