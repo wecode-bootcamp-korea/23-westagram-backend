259ff0b0dd103686c6902fb20b7c207eaae23a05
preferbaik <baik9261@gmail.com>
2021-07-22 18:12:37 +0900

    project model

---
+++ students/sunhobaik/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import TextField, DateField, IntegerField
+
+
+class User(models.Model):
+    name=models.TextField(max_length=20)
+    email=models.TextField(max_length=100)
+    password=models.TextField(max_length=20)
+    phone_numbers=models.IntegerField()
+    privacy=models.TextField(max_length=200)
+    
+    def Meta(self):
+        db_table = 'users'
+
+
