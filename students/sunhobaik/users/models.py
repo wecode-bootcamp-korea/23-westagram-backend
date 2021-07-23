810c01c39334e1e19d78e7c0445c1d49a014a5f4
preferbaik <baik9261@gmail.com>
2021-07-23 14:42:01 +0900

    project model7

--- students/sunhobaik/users/models.py
+++ students/sunhobaik/users/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
-
 
 
 class User(models.Model):
     name          = models.CharField(max_length=45)
-    email         = models.TextField(max_length=200)
-    password      = models.TextField(max_length=45)
+    email         = models.CharField(max_length=200)
+    password      = models.CharField(max_length=45)
     phone_number  = models.CharField(max_length=45)
-    web_site      = models.TextField(max_length=200)
+    web_site      = models.CharField(max_length=200)
     nick_name     = models.CharField(max_length=45) 
-    address       = models.TextField(max_length=200)
-    introduce     = models.CharField(max_length=4000)
+    address       = models.CharField(max_length=200)
+    introduce     = models.TextField()
 
     class Meta:
         db_table = 'users'
