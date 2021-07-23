e434e3e8589280baba047974b0f031b4c4790c43
preferbaik <baik9261@gmail.com>
2021-07-23 16:12:57 +0900

    project view2

--- students/sunhobaik/users/views.py
+++ students/sunhobaik/users/views.py
@@ -8,11 +8,14 @@ class UserView(View):
     def post(self, request):
         data=json.loads(request.body)
         Email = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-z0-9-]+\.[a-zA-z0-9-]+$")
-        PW = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
-        if PW.match(data['password']) == None and Email.match(data['email']) == None:
+        PW    = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+        
+        if  PW.match(data['password'])  == None and Email.match(data['email']) == None:
             return JsonResponse({'message':'Please check your password and email address!'}, status=400)         
+        
         elif Email.match(data['email']) == None :
             return JsonResponse({'message':'Please check your email address!'}, status=400)   
+        
         elif PW.match(data['password']) == None :
             return JsonResponse({'message':'Please check your password!'}, status=400)  
         else:
@@ -27,3 +30,4 @@ class UserView(View):
                 introduce     = data['introduce']
             )
             return JsonResponse({'message': "SUCCESS"}, status=201)
+
