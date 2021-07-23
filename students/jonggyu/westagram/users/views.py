107777e58747a32096a2943a1db012a2799a3653
dwenpark <dwenup@gmail.com>
2021-07-23 16:40:13 +0900

    Modify: else 제거, except에 keyerror 명시

--- students/jonggyu/westagram/users/views.py
+++ students/jonggyu/westagram/users/views.py
@@ -7,21 +7,21 @@ from .models        import User
 
 class UserView(View):
     def post(self, request):
-        data = json.loads(request.body)
-
-        if (data["password"] == "") or (data["email"] == ""):
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        if re.match(r"^[a-zA-Z0-9+._-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]{2,4}$", data["email"]) == None:
-            return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
-
-        if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$" ,data["password"]) == None:
-            return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
-
-        if User.objects.filter(email = data["email"]).exists():
-            return JsonResponse({"message": "RESISTERED_EMAIL"}, status=400)
-
         try:
+            data = json.loads(request.body)
+
+            if (data["password"] == "") or (data["email"] == ""):
+                return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+            if re.match(r"^[a-zA-Z0-9+._-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]{2,4}$", data["email"]) is None:
+                return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
+
+            if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$" ,data["password"]) is None:
+                return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
+
+            if User.objects.filter(email = data["email"]).exists():
+                return JsonResponse({"message": "RESISTERED_EMAIL"}, status=400)
+
             User.objects.create(
                 name          = data["name"],
                 email         = data["email"],
@@ -29,9 +29,8 @@ class UserView(View):
                 phone_number  = data["phone_number"],
                 age           = data["age"]
             )
-
-        except:
-            return JsonResponse({"message": "EMPTY_INFORMATION"}, status=400)
-
-        else:
             return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
