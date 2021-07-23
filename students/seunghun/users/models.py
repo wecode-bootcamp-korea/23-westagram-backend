b29f82958c3d7d2e1f9d4d4d5c1da102b4295495
Samdaso-o <soc05214@naver.com>
2021-07-23 13:58:09 +0900

    project_mission2_3

--- students/seunghun/users/models.py
+++ students/seunghun/users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=10)
-    email = models.CharField(max_length=45)
-    password = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=50, null=True)
-    date_of_birth = models.DateField(null=True)
-    nickname = models.CharField(max_length=10,null=True)
+    name          = models.CharField(max_length=45)
+    email         = models.CharField(max_length=200)
+    password      = models.CharField(max_length=200)
+    phone_number  = models.CharField(max_length=45)
+    address       = models.CharField(max_length=200, null=True)
+    birth_date    = models.DateField(null=True)
+    nickname      = models.CharField(max_length=45,  null=True)
 
     class Meta:
         db_table = 'users'
