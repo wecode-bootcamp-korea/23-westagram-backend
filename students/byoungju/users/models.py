e8e350f2e229cad5e3bcb07624e732cb621c275b
byoungjupark <byoungju1012@gmail.com>
2021-07-23 15:19:18 +0900

    Update: models variables2

--- students/byoungju/users/models.py
+++ students/byoungju/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name     = models.CharField(max_length=45)
-    email    = models.CharField(max_length=200)
-    password = models.CharField(max_length=45)
-    phone    = models.CharField(max_length=45)
-    birth_date    = models.DateField()
+    name            = models.CharField(max_length=45)
+    email           = models.CharField(max_length=200)
+    password        = models.CharField(max_length=45)
+    phone_number    = models.CharField(max_length=45)
+    birth_date      = models.DateField()
 
     class Meta:
         db_table = "users"
