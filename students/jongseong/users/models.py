9ee360bd286fd0205ba65321a5a1e59c19b048d8
임종성 <qkqndudnxld@gmail.com>
2021-07-23 15:58:28 +0900

    Add: Models

---
+++ students/jongseong/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name         = models.CharField(max_length=45)
+    email        = models.CharField(max_length=200, unique=True)
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=45)
+    age          = models.PositiveIntegerField(max_length=3)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
+
