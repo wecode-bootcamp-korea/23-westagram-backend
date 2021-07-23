d9816cfc5c27f9559c09537dfdd7b912b290cf27
임종성 <qkqndudnxld@gmail.com>
2021-07-23 17:58:10 +0900

    Add: Views

--- students/jongseong/users/models.py
+++ students/jongseong/users/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     email        = models.CharField(max_length=200, unique=True)
     password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=45)
-    age          = models.PositiveIntegerField(max_length=3)
+    age          = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'users'
