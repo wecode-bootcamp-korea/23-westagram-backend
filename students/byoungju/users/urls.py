c2d033740684cf028bdfc460704cdf64cae653f7
byoungjupark <byoungju1012@gmail.com>
2021-07-23 17:53:58 +0900

    Update: views, urls2

--- students/byoungju/users/urls.py
+++ students/byoungju/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from users.views import UserView
 
 urlpatterns = [
-    path("user", UserView.as_view()),
+    path("", UserView.as_view()),
 ]
--- students/byoungju/users/views.py
+++ students/byoungju/users/views.py
@@ -11,24 +11,25 @@ class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        if ((data["email"] or data["password"]) not in data.values()):
+        try:
+            if (re.match("\w*@\w*.\w*", data["email"]) is None):
+                return JsonResponse({"message":"INVALID__EMAIL"}, status = 400)
+
+            if (re.match("[\w\W{8,20}]", data["password"]) is None):
+                return JsonResponse({"message":"INVALID_PASSWORD"}, status = 400)  
+            
+            if User.objects.filter(email = data["email"]).exists():
+                return JsonResponse({"message":"EXIST_EMAIL"}, status = 400)
+
+            User.objects.create(
+                name         = data["name"],
+                email        = data["email"],
+                password     = data["password"],
+                phone_number = data["phone_number"],
+                birth_date   = data["birth_date"]        
+            )
+
+            return JsonResponse({"message":"SUCCESS"}, status = 201)
+
+        except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status = 400)
-
-        if (re.match("\w*@\w*.\w*", data["email"]) == None):
-            return JsonResponse({"message":"INVALID__EMAIL"}, status = 400)
-
-        if (re.match("[\w\W{8,20}]", data["password"]) == None):
-            return JsonResponse({"message":"INVALID_PASSWORD"}, status = 400)  
-        
-        if User.objects.filter(email = data["email"]).exists():
-            return JsonResponse({"message":"EXIST_EMAIL"}, status = 400)
-
-        User.objects.create(
-            name         = data["name"],
-            email        = data["email"],
-            password     = data["password"],
-            phone_number = data["phone_number"],
-            birth_date   = data["birth_date"]        
-        )
-
-        return JsonResponse({"message":"SUCCESS"}, status = 201)
