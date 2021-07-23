48aebbcb5f974105d119af65a800b6968cb22a21
Jang184 <e2joo418@gmail.com>
2021-07-23 14:17:24 +0900

    Edit2 : models.py

--- students/yiju/users/models.py
+++ students/yiju/users/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 
 class User(models.Model) :
-	name          = models.CharField(max_length=45)
-	email         = models.CharField(max_length=100, unique=True)
-	password      = models.CharField(max_length=100)
-	
+	name      = models.CharField(max_length=45)
+	email     = models.CharField(max_length=100, unique=True)
+	password  = models.CharField(max_length=100)
+	contact   = models.CharField(max_length=100, null=True)
 
 	class Meta :
 		db_table = 'users'
-
-class Information(models.Model) :
-	contact_point = models.CharField(max_length=100, null=True)
-	sex            = models.CharField(max_length=3, null=True)
-	age            = models.PositiveIntegerField(null=True)
-	user           = models.ForeignKey('User',on_delete = models.CASCADE)
-
-	class Meta :
-		db_table = 'information'
