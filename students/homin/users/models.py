0f55e144445d4846dbf248eafc6d7c40473baa6e
shm <thsrns3934@gmail.com>
2021-07-23 15:26:38 +0900

    Add: user model

--- students/homin/users/models.py
+++ students/homin/users/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.EmailField(max_length=320)
-    password = models.CharField(max_length=45)
-    phonenumber = models.CharField(max_length=45)
-#    birth_date = models.DateField(null=True, blank=True)
-#    time_zone = models.CharField(max_length=45, black=True)
-#    regist_date = models.DateField()
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=320)
+    password     = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
     class Meta:
         db_table = 'users'
     def __str__(self):
