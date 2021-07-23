4cf4bfa4edc568a0a8c12fc6ba6836639d29d1c7
damdream <possible_11@naver.com>
2021-07-23 11:23:15 +0900

    models.py 2차 수정

--- students/dodam/westagram/users/models.py
+++ students/dodam/westagram/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
-	name = models.CharField(max_length=45)
-	email = models.CharField(max_length=200)
-	password = models.CharField(max_length=200)
+	name 		 = models.CharField(max_length=45)
+	email	 	 = models.CharField(max_length=200, unique=True)
+	password 	 = models.CharField(max_length=200, null=False)
 	phone_number = models.CharField(max_length=20)
-	birthday = models.DateTimeField('date published')
+	birthday	 = models.DateTimeField()
 
 	class Meta:
-		db_table = 'users'
+	   db_table = 'users'
