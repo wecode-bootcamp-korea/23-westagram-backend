71937a392196de72b00e45a88ec52d4110a74a7f
Samdaso-o <soc05214@naver.com>
2021-07-23 11:24:02 +0900

    project_mission2_2

--- students/seunghun/users/models.py
+++ students/seunghun/users/models.py
@@ -1,22 +1,15 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
-class Essential_User(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=10)
     email = models.CharField(max_length=45)
     password = models.CharField(max_length=20)
-    phone_num = models.CharField(max_length=20)
-
-    class Meta:
-        db_table = 'essential_users'
-
-class Select_User(models.Model):
-    name = models.ForeignKey('Essential_User', on_delete=CASCADE)
+    phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50, null=True)
     date_of_birth = models.DateField(null=True)
     nickname = models.CharField(max_length=10,null=True)
 
     class Meta:
-        db_table = 'select_users'
+        db_table = 'users'
