4eb0eb655983cf112b7eabca68f4a3dd52721851
damdream <possible_11@naver.com>
2021-07-23 14:10:13 +0900

    models.py null삭제

--- students/dodam/westagram/users/models.py
+++ students/dodam/westagram/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
 	name 		 = models.CharField(max_length=45)
 	email	 	 = models.CharField(max_length=200, unique=True)
-	password 	 = models.CharField(max_length=200, null=False)
+	password 	 = models.CharField(max_length=200)
 	phone_number = models.CharField(max_length=20)
 	birthday	 = models.DateTimeField()
 
