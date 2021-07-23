5a7871a19d85ddd622224f0ea94d6cfc80b9fc8f
jake <tkddnd82@gmail.com>
2021-07-23 19:45:07 +0900

    signup-setting2

--- students/sangwoong/westagram/users/view.py
+++ students/sangwoong/westagram/users/view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-import json   # 프론트로부터 javascript 데이터 파일을 받기 위해
+
+import json
 
 from django.http  import JsonResponse 
 from django.views import View
@@ -17,19 +17,19 @@ class UserView(View):
             MINIMUM_PASSWORD_LENGTH = 8
             if '@' not in data['email'] or '.' not in data['email']:
                 return JsonResponse({'MESSAGE' : 'INVALID EMAIL'}, status=400)
-			#먄약 @나 .이 없으면 오류메세지 날림.
+
             if len(data['password']) < MINIMUM_PASSWORD_LENGTH:
                 return JsonResponse({'MESSAGE' : 'INVALID PASSWORD'}, status=400)
-            #만약 8자리이상 문자 숫자 특수문자 조합이어야함 충족되지 않으면 오류 메세지를 날림.
+
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSAGE' : 'DUPLICATED EMAIL'}, status=400)
-            #만약 이메일 중복된다면 오류 메세지 날림.
+
             if User.objects.filter(phone_number=data['phone_number']).exists():
                 return JsonResponse({'MESSAGE' : 'DUPLICATED PHONE_NUM'}, status=400)
-            #만약 이름 중복된다면 오류 메세지 날림.
+
             if User.objects.filter(name=data['name']).exists():
                 return JsonResponse({'MESSAGE' : 'DUPLICATED NAME'}, status=400)
-			#만약 폰번호 중복된다면 오류 메세지 날림.	
+
               
             User.objects.create(
                 name      = data['name'],
