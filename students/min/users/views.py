fee974d008a547e06660094b0f5055952d4e6f41
최민 <cm9542@gmail.com>
2021-07-23 17:01:52 +0900

    Update complete

--- students/min/users/views.py
+++ students/min/users/views.py
@@ -16,31 +16,34 @@ def password_validation(password):
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
+        
+        try:
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+            if email_validation(email): 
+                return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status=400)
+            
+            if password_validation(password): 
+                return JsonResponse({"MESSAGE":"INVALID_PASSWORD"}, status=400)
+            
+            if User.objects.filter(email=email).exists(): 
+                return JsonResponse({"MESSAGE":"ALREADY_EMAIL_EXSISTS"}, status=400)
 
-        if not ('email' or 'password') in data:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+            if User.objects.filter(phone_number=phone_number).exists(): 
+                return JsonResponse({"MESSAGE":"ALREADY_PHONE_NUMBER_EXSISTS"}, status=400)
         
-        if email_validation(data['email']):
-            return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status=400)
-        
-        if password_validation(data['password']):
-            return JsonResponse({"MESSAGE":"INVALID_PASSWORD"}, status=400)
-        
-        if User.objects.filter(email=data['email']).exists():
-            return JsonResponse({"MESSAGE":"ALREADY_EMAIL_EXSISTS"}, status=400)
-
-        if User.objects.filter(phone_number=data['phone_number']).exists():
-            return JsonResponse({"MESSAGE":"ALREADY_PHONE_NUMBER_EXSISTS"}, status=400)
-        
-        else:
-            signup_user = User.objects.create(
-                name          = data['name'],
-                email         = data['email'],
-                password      = data['password'],
-                phone_number  = data['phone_number'],
-                age           = data['age'],
-                gender        = data['gender'],
-                birth_date    = data['birth_date']
+            User.objects.create(
+                name         = data['name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
+                age          = data['age'],
+                gender       = data['gender'],
+                birth_date   = data['birth_date']
             )
             return JsonResponse({"MESSAGE": "SUCCESS"}, status=201)
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"},status=400)
+
         
