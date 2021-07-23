459afedd7b87976f9e4d9871f00162ea25ee642c
lunayyko <youryoung.ko@gmail.com>
2021-07-23 15:49:58 +0900

    EDIT: 핸드폰 데이터필드 이름, 컨벤션

--- students/yooyoung/users/models.py
+++ students/yooyoung/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=40)
-    email = models.EmailField(max_length=200, unique=True)
-    password = models.CharField(max_length=80)
-    age = models.PositiveIntegerField(null=True)
-    phone = models.CharField(max_length=16, null=True)
+    name         = models.CharField(max_length=40)
+    email        = models.EmailField(max_length=200, unique=True)
+    password     = models.CharField(max_length=200)
+    age          = models.PositiveIntegerField(null=True)
+    phone_number = models.CharField(max_length=60, null=True)
 
     class Meta:
         db_table = 'users'
