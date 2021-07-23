6539d0661eb4e067f2a955451b1319d1f7880a79
preferbaik <baik9261@gmail.com>
2021-07-23 16:06:49 +0900

    project view1

--- students/sunhobaik/users/models.py
+++ students/sunhobaik/users/models.py
@@ -5,11 +5,11 @@ class User(models.Model):
     name          = models.CharField(max_length=45)
     email         = models.CharField(max_length=200)
     password      = models.CharField(max_length=45)
-    phone_number  = models.CharField(max_length=45)
-    web_site      = models.CharField(max_length=200)
-    nick_name     = models.CharField(max_length=45) 
-    address       = models.CharField(max_length=200)
-    introduce     = models.TextField()
+    phone_number  = models.CharField(max_length=45, null=True)
+    web_site      = models.CharField(max_length=200, null=True)
+    nick_name     = models.CharField(max_length=45, null=True) 
+    address       = models.CharField(max_length=200, null=True)
+    introduce     = models.TextField(null=True, default='')
 
     class Meta:
         db_table = 'users'
--- students/sunhobaik/users/views.py
+++ students/sunhobaik/users/views.py
@@ -7,33 +7,23 @@ import re
 class UserView(View):
     def post(self, request):
         data=json.loads(request.body)
-        # p = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-z0-9-]+\.[a-zA-z0-9-]+$")
-        # # q = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
-        # if p.match(data['email']) == None :
-        #     return JsonResponse({'message':'check your email address'}, status=404)   
-        # else:
-        #     User.objects.create(
-        #         name          = data['name'],
-        #         email         = data['email'],
-        #         password      = data['password'],
-        #         phone_numbers = data['phone_numbers'],
-        #         web_site      = data['website'],
-        #         nick_name     = data['nick_name'],
-        #         address       = data['address'],
-        #         introduce     = data['introduce']
-        #     )
-        #     return JsonResponse({'message': 'created'}, status=201)
-        # elif data['email'] =='' or data['password']=='' or  data['address']=='' or  data['name']=='':
-        #     return JsonResponse({'message':'check your profile'}, status=404)
-
-        User.objects.create(
-            name          = data['name'],
-            email         = data['email'],
-            password      = data['password'],
-            phone_numbers = data['phone_numbers'],
-            web_site      = data['web_site'],
-            nick_name     = data['nick_name'],
-            address       = data['address'],
-            introduce     = data['introduce']
-        )
-        return JsonResponse({'message': 'created'}, status=201)
+        Email = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-z0-9-]+\.[a-zA-z0-9-]+$")
+        PW = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+        if PW.match(data['password']) == None and Email.match(data['email']) == None:
+            return JsonResponse({'message':'Please check your password and email address!'}, status=400)         
+        elif Email.match(data['email']) == None :
+            return JsonResponse({'message':'Please check your email address!'}, status=400)   
+        elif PW.match(data['password']) == None :
+            return JsonResponse({'message':'Please check your password!'}, status=400)  
+        else:
+            User.objects.create(
+                name          = data['name'],
+                email         = data['email'],
+                password      = data['password'],
+                phone_numbers = data['phone_numbers'],
+                web_site      = data['website'],
+                nick_name     = data['nick_name'],
+                address       = data['address'],
+                introduce     = data['introduce']
+            )
+            return JsonResponse({'message': "SUCCESS"}, status=201)
