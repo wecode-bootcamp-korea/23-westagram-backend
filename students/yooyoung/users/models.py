b5bdc25022c45dc4b0a95a1a08d2aa10d29df21b
lunayyko <youryoung.ko@gmail.com>
2021-07-23 14:26:04 +0900

    ADD: users/Models에 User class 추가

--- students/yooyoung/users/models.py
+++ students/yooyoung/users/models.py
@@ -1,23 +1,12 @@
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=80)
     note = models.TextField(max_length=100, null=True)
-    phone = models.CharField(
-        max_length=16,
-        blank=True,
-        null=True,
-        validators=[
-            RegexValidator(
-                regex=r'^\+?1?\d{9,15}$',
-                message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
-            ),
-        ],
-    )
-    
+    phone = models.CharField(max_length=16, null=True)
+
     class Meta:
-        db_table = 'Users'
+        db_table = 'users'
