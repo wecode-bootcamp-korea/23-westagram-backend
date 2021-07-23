591b591fcec7a6fbd42428d7f95882c71e08043f
Jang184 <e2joo418@gmail.com>
2021-07-23 17:06:27 +0900

    Add : users views.py

--- students/yiju/users/models.py
+++ students/yiju/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model) :
 	name      = models.CharField(max_length=45)
 	email     = models.CharField(max_length=100, unique=True)
 	password  = models.CharField(max_length=100)
-	contact   = models.CharField(max_length=100, null=True)
+	contact   = models.CharField(max_length=100, blank=True, default='')
 
 	class Meta :
 		db_table = 'users'
--- students/yiju/users/views.py
+++ students/yiju/users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class UserView(View) :
+	def post (self, request) :
+		try : 
+			data     = json.loads(request.body)
+			email    = data['email']
+			password = data['password']
+
+			email_regx = r'[\w\.-]{3,15}@[\w\.-]{2,}\.\w{2,4}\b'
+			email_val  = re.match(email_regx, email)
+			if not email_val :
+				return JsonResponse({'message':'wrong_email'}, status=400)
+
+			password_regx = r'^(?=(.*[A-Za-z]))(?=(.*[0-9]))(?=(.*[@#$%^!&+=.\-_*]))([a-zA-Z0-9@#$%^!&+=*.\-_]){8,}$'
+			password_var  = re.match(password_regx, password)
+			if not password_var :
+				return JsonResponse({'message':'wrong_pasword'}, status=400)
+
+			if User.objects.filter(email=email).exists() :
+				return JsonResponse({'message':'email_already_exists'}, status = 400)
+
+			User.objects.create(
+				name     = data['name'],
+				email    = email,
+				password = password,
+				contact  = data['contact']
+				)	
+			return JsonResponse({'message':'success'}, status=201)
+
+		except KeyError :
+			return JsonResponse({'message':'key_error'}, status = 400)
+
+		
+
+		
