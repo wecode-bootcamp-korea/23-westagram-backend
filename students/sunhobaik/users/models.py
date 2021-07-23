e5c7580d01fc9b6ce154700c8ed549fdd12025f9
preferbaik <baik9261@gmail.com>
2021-07-23 14:11:24 +0900

    project model5

--- students/sunhobaik/users/models.py
+++ students/sunhobaik/users/models.py
@@ -4,15 +4,16 @@ from django.db.models.deletion import CASCADE
 
 
 class User(models.Model):
-    name          = models.CharField(max_length=20)
-    email         = models.TextField(max_length=100)
-    password      = models.TextField(max_length=20)
-    phone_numbers = models.CharField(max_length=20)
-    web_site      = models.TextField(max_length=30)
-    nick_name     = models.CharField(max_length=20) 
-    address       = models.TextField(max_length=100)
-    introduce     = models.TextField(max_length=200)
-    def Meta(self):
+    name          = models.CharField(max_length=45)
+    email         = models.TextField(max_length=200)
+    password      = models.TextField(max_length=45)
+    phone_numbers = models.CharField(max_length=45)
+    web_site      = models.TextField(max_length=200)
+    nick_name     = models.CharField(max_length=45) 
+    address       = models.TextField(max_length=200)
+    introduce     = models.CharField(max_length=4000)
+
+    class Meta:
         db_table = 'users'
 
 
---
+++ students/sunhobaik/users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+from users.views import UserView
+urlpatterns = [
+    path('', UserView.as_view()),
+]
