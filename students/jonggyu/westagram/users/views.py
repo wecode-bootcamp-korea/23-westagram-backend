97154208718d0015ce47ef679ef8a75cdba3e3cd
dwenpark <dwenup@gmail.com>
2021-07-23 16:18:18 +0900

    Add: 회원가입 view에 try,except 적용

--- students/jonggyu/westagram/users/views.py
+++ students/jonggyu/westagram/users/views.py
@@ -1,30 +1,37 @@
+import json, re
+
 from django.views   import View
 from django.http    import JsonResponse
+
 from .models        import User
-import json
-import re
 
 class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        if (data["email"] == "") or (data["password"] == ""):
-            return JsonResponse({"message": "ERROR_EMPTY_EMAIL_PASSWORD"}, status=400)
+        if (data["password"] == "") or (data["email"] == ""):
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
         if re.match(r"^[a-zA-Z0-9+._-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]{2,4}$", data["email"]) == None:
-            return JsonResponse({"message": "ERROR_EMAIL_NEED_@AND."}, status=400)
+            return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
 
         if re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$" ,data["password"]) == None:
-            return JsonResponse({"message": "ERROR_PASSWORD(NEED_OVER8_AND_ALPHABET,NUMBER,SPECIAL_SYMBOLS)"}, status=400)
+            return JsonResponse({"message": "INVALID_FORMAT"}, status=400)
 
-        if User.objects.filter(email = data["email"]).count() == 1:
-            return JsonResponse({"message": "ERROR_RESISTERED_EMAIL"}, status=400)
+        if User.objects.filter(email = data["email"]).exists():
+            return JsonResponse({"message": "RESISTERED_EMAIL"}, status=400)
 
-        User.objects.create(
-            name          = data["name"],
-            email         = data["email"],
-            password      = data["password"],
-            phone_number  = data["phone_number"],
-            age           = data["age"]
-        )
-        return JsonResponse({"message" : "SUCCESS"}, status=201)
+        try:
+            User.objects.create(
+                name          = data["name"],
+                email         = data["email"],
+                password      = data["password"],
+                phone_number  = data["phone_number"],
+                age           = data["age"]
+            )
+
+        except:
+            return JsonResponse({"message": "EMPTY_INFORMATION"}, status=400)
+
+        else:
+            return JsonResponse({"message": "SUCCESS"}, status=201)
