9413dc86cea82eb39626e20ed89b0156b84b97b4
byoungjupark <byoungju1012@gmail.com>
2021-07-23 20:06:59 +0900

    Update: login view

--- students/byoungju/login/views.py
+++ students/byoungju/login/views.py
@@ -3,19 +3,27 @@ import re
 
 from django.http import JsonResponse
 from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
 
 from users.models import User
 
 class LoginView(View):
     def post(sef, request):
-        data = json.loads(request.body)
+        data     = json.loads(request.body)
+        email    = data["email"]
+        password = data["password"]
 
         try:
-            if (User.objects.filter(email = data["email"]).exists() and \
-                User.objects.filter(password = data["password"]).exists()):
+
+            if (User.objects.get(email = email) and \
+                User.objects.filter(password = password).exists()):
                 return JsonResponse({"message":"SUCCESS"}, status = 200)
             else:
                 return JsonResponse({"message":"INVALID_USER"}, status = 401)
             
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+        except ObjectDoesNotExist:
+            return JsonResponse({"message":"INVALID_USER"}, status = 401)
+
