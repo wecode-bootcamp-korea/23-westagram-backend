01cc1c3c641627195f346574dc980900b0369a67
damdream <possible_11@naver.com>
2021-07-23 23:38:50 +0900

    로그인 기능 구현 1차

--- students/dodam/westagram/users/views.py
+++ students/dodam/westagram/users/views.py
@@ -10,15 +10,12 @@ password_regular = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d
 class UserView(View):
 	def post(self,request):
 		try:	
-			data = json.loads (request.body) # 프론트가 requset한 정보를 바디에 담아 json 을 활용해 읽어내겠다는 의미 
-			# 중복에러 설정하기
+			data = json.loads(request.body)  
 			if User.objects.filter(name=data['name']).exists() or User.objects.filter(email=data['email']).exists(): 			
 				return JsonResponse({'MESSAGE': 'DATA_OVERLAP'}, status = 400)		
 
-			# 이메일 필수조건 설정 및 패스워드 설정 관련 정규식 : 8자이상, 문자, 숫자, 특수문자 조건 충족
 			if ('@' not in data['email']) or ('.'not in data['email']) or (not password_regular.match(data['password'])):
 				return JsonResponse ({'MESSAGE': 'INVALID_FORMAT'}, status = 400)
-
 
 			User.objects.create(
 			name 		 = data['name'],
@@ -27,7 +24,20 @@ class UserView(View):
 			phone_number = data['phone_number'], 
 			birthday 	 = data['birthday'],
 			)
-			return JsonResponse ({'MESSAGE' : 'SUCCESS'}, status = 201)
+			return JsonResponse ({'MESSAGE':'SUCCESS'}, status = 201)
 
-		except KeyError: # 키 값이 빠진다면 나오는 오류
-			return JsonResponse ({'MESSAGE': 'KEY_ERROR'}, status = 400)
+		except KeyError: 
+			return JsonResponse ({'MESSAGE':'KEY_ERROR'}, status = 400)
+
+class LoginView(View):
+	def post(self,requset):
+		try:
+			data = json.loads(requset.body)
+			if (data['email']) or (data['password']) == "" :
+				return JsonResponse ({'MESSAGE': 'KEY_ERROR'},status = 400)	
+			if not (User.objects.filter(data['email']).exists()) or data['password'] == (User.objects.filter(data['password'])):
+				return JsonResponse ({'MESSAGE':'INVALID_USER'}, status =401)
+			return JsonResponse ({'MESSAGE':'SUCCESS'}, status = 200)
+
+		except KeyError:
+			return JsonResponse ({'MESSAGE':'KEY_ERROR'},status = 400)	
