741a361d368495f86f37ce35c766092c8178da5a
preferbaik <baik9261@gmail.com>
2021-07-23 14:32:11 +0900

    project model6

--- students/sunhobaik/users/models.py
+++ students/sunhobaik/users/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     name          = models.CharField(max_length=45)
     email         = models.TextField(max_length=200)
     password      = models.TextField(max_length=45)
-    phone_numbers = models.CharField(max_length=45)
+    phone_number  = models.CharField(max_length=45)
     web_site      = models.TextField(max_length=200)
     nick_name     = models.CharField(max_length=45) 
     address       = models.TextField(max_length=200)
