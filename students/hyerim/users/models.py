4053e42d4bfc69ba11d99086297a6240d4c98cf7
choihyerim <hyerimc858@gmail.com>
2021-07-23 11:13:10 +0900

    Modify3: users models, settings.py

--- students/hyerim/users/models.py
+++ students/hyerim/users/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
+    name       = models.CharField(max_length=45)
+    email      = models.CharField(max_length=100)
+    password   = models.CharField(max_length=100)
+    mobile     = models.CharField(max_length=100)
+    address    = models.CharField(max_length=100)
     birth_date = models.DateField()
 
     class Meta:
-        db_table="user_info"
+        db_table="users"
