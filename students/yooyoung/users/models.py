bbad86a3254b3184d5725205e2e8ccac5e279db7
lunayyko <youryoung.ko@gmail.com>
2021-07-23 15:03:43 +0900

    EDIT: note(textfield)-> age(positiveintegerfield)

--- students/yooyoung/users/models.py
+++ students/yooyoung/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=200, unique=True)
     password = models.CharField(max_length=80)
-    note = models.TextField(max_length=100, null=True)
+    age = models.PositiveIntegerField(null=True)
     phone = models.CharField(max_length=16, null=True)
 
     class Meta:
