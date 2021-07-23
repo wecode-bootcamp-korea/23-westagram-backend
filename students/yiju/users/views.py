f9eeb35f4b2887b0a212809a0d903c985b377fc8
Jang184 <e2joo418@gmail.com>
2021-07-23 17:27:39 +0900

    Edit : views.py

--- students/yiju/users/views.py
+++ students/yiju/users/views.py
@@ -6,25 +6,27 @@ from django.views import View
 
 from users.models import User
 
-class UserView(View) :
-	def post (self, request) :
-		try : 
+class UserView(View):
+	def post (self, request):
+		try: 
 			data     = json.loads(request.body)
 			email    = data['email']
 			password = data['password']
 
-			email_regx = r'[\w\.-]{3,15}@[\w\.-]{2,}\.\w{2,4}\b'
-			email_val  = re.match(email_regx, email)
-			if not email_val :
+			email_regx    = r'[\w\.-]{3,15}@[\w\.-]{2,}\.\w{2,4}\b'
+			password_regx = r'^(?=(.*[A-Za-z]))(?=(.*[0-9]))(?=(.*[@#$%^!&+=.\-_*]))([a-zA-Z0-9@#$%^!&+=*.\-_]){8,}$'
+
+			email_val     = re.match(email_regx, email)
+			password_val  = re.match(password_regx, password)
+
+			if not email_val:
 				return JsonResponse({'message':'wrong_email'}, status=400)
 
-			password_regx = r'^(?=(.*[A-Za-z]))(?=(.*[0-9]))(?=(.*[@#$%^!&+=.\-_*]))([a-zA-Z0-9@#$%^!&+=*.\-_]){8,}$'
-			password_var  = re.match(password_regx, password)
-			if not password_var :
+			if not password_val:
 				return JsonResponse({'message':'wrong_pasword'}, status=400)
 
-			if User.objects.filter(email=email).exists() :
-				return JsonResponse({'message':'email_already_exists'}, status = 400)
+			if User.objects.filter(email=email).exists():
+				return JsonResponse({'message':'email_already_exists'}, status=400)
 
 			User.objects.create(
 				name     = data['name'],
@@ -34,8 +36,8 @@ class UserView(View) :
 				)	
 			return JsonResponse({'message':'success'}, status=201)
 
-		except KeyError :
-			return JsonResponse({'message':'key_error'}, status = 400)
+		except KeyError:
+			return JsonResponse({'message':'key_error'}, status=400)
 
 		
 
