d733a9383acafa90ccaaa44f2f0d1518d0b6329e
shm <thsrns3934@gmail.com>
2021-07-26 14:09:33 +0900

    Add: signup

--- students/homin/users/views.py
+++ students/homin/users/views.py
@@ -12,7 +12,6 @@ class ValidationError(Exception):
     def __str__(self):
         return self.msg
 
-
 class UserView(View):
     class ValidationError(Exception):
         def __init__(self, msg):
@@ -24,30 +23,30 @@ class UserView(View):
         try:
             data = json.loads(request.body)
 
-            if not re.match('^\w+@\w+\.\l+$', data['email']):
+            if not re.match('^\w+@\w+\.\w+$', data['email']):
                 raise ValidationError('INVALID_EMAIL_FORMAT')
 
-            if (not re.match('\S{1:8}', data['password']) or
+            if (not re.match('\S{1,8}', data['password']) or
                     len(data['password']) < 8):
                 raise ValidationError('INVALID_PASSWORD_FORMAT')
 
             if not re.match('\d{3}-\d{3,4}-\d{4}', data['phone_number']):
                 raise ValidationError('INVALID_PHONE_NUMBER_FORMAT')
 
-            if User.objects.filter(email=data['email']).exist():
+            if User.objects.filter(email=data['email']).exists():
                 raise ValidationError('EXISTED_EMAIL')
 
         except ValidationError as e:
-            return JsonResponse({"message": e}, status=400)
+            return JsonResponse({"message": e.msg}, status=400)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
-#        except Exception:
-#            return JsonResponse({"message": "INVALID_VALUE"}, status=400)
+        except Exception:
+            return JsonResponse({"message": "INVALID_VALUE"}, status=400)
         
         else:
-            user.objects.create(
+            User.objects.create(
                 name=data['name'],
                 email=data['email'],
                 password=data['password'],
