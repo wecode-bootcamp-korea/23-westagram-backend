f23aa6ed1b14c6b8797e51cf223acb5a1748d192
shm <thsrns3934@gmail.com>
2021-07-26 17:38:07 +0900

    Add: signin

--- students/homin/users/views.py
+++ students/homin/users/views.py
@@ -49,9 +49,6 @@ class SigninView(View):
             if  data['password'] != User.objects.get(email.data['email']).password:
                 return jsonresponse({"message": "INVALID_USER"}, status=401)
 
-            if not User.objects.filter(email=data['email'], password=data['password']).exists():
-                return JsonResponse({"message": "INVALID_USER"}, status=401)
-            
             return JsonResponse({"message": "SUCCESS"}, status=200)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
