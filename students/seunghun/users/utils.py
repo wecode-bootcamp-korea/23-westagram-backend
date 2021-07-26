82fd3693a8271d67ee8fb3cf515d20f56c52bd7f
Samdaso-o <soc05214@naver.com>
2021-07-26 20:03:09 +0900

    project_mission6_1

--- students/seunghun/users/utils.py
+++ students/seunghun/users/utils.py
@@ -8,7 +8,7 @@ from westagram.settings import SECRET_KEY
 def Keep_signup(func):
     def wrap(self,request):
         receive_token = request.header.get('Authorization')
-        receive_id = jwt.decode(receive_id, SECRET_KEY, algorithms='HS256')
+        receive_id = jwt.decode(receive_token, SECRET_KEY, algorithms='HS256')
         if receive_id == User.objects.get(id):
             return JsonResponse({'MESSAGE':'VALID_TOKEN'}, status=200)
         return JsonResponse({'MESSAGE':'INVALID_TOKEN'}, status=400)
