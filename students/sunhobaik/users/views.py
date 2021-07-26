f809d119f9b8b9a070d23eaad87403d2dad423ef
preferbaik <baik9261@gmail.com>
2021-07-26 16:26:55 +0900

    project bcrypt1

--- students/sunhobaik/users/views.py
+++ students/sunhobaik/users/views.py
@@ -3,14 +3,17 @@ from django.views import View
 import json
 from users.models import User
 import re
+import bcrypt
 
 class UserView(View):
     def post(self, request):
         try:
-            data  = json.loads(request.body)
-            Email = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-z0-9-]+\.[a-zA-z0-9-]+$")
-            PW    = re.compile("/^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,20}$/")
-       
+            data       = json.loads(request.body)
+            print(data)
+            Email      = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-z0-9-]+\.[a-zA-z0-9-]+$")
+            PW         = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,16}$")
+            encoded_pw = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            
             if  PW.match(data['password'])  is None or Email.match(data['email']) is None:
                 return JsonResponse({'message':' INVALID_FORMAT'}, status=400)         
             
@@ -20,32 +23,31 @@ class UserView(View):
             User.objects.create(
                 name          = data['name'],
                 email         = data['email'],
-                password      = data['password'],
-                phone_numbers = data['phone_numbers'],
-                web_site      = data['website'],
-                nick_name     = data['nick_name'],
-                address       = data['address'],
-                introduce     = data['introduce']
+                password      = encoded_pw.decode('utf-8'),
+                phone_number  = data.get('phone_number'),
+                web_site      = data.get('website'),
+                nick_name     = data.get('nick_name'),
+                address       = data.get('address'),
+                introduce     = data.get('introduce')
             )           
             return JsonResponse({'message': "SUCCESS"}, status=201)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-
 class LoginView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+            user = User.objects.filter(email=data['email'])
             
-            if not User.objects.filter(email=data['email']).exists():
+            if user.exists():
+                if bcrypt.checkpw(data['password'].encode('utf-8'), user[0].password.encode('utf-8')):
+                    return JsonResponse({'message': "SUCCESS"}, status=200)              
+                
+            else:
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
-            
-            if User.objects.get(email=data['email']).password != data['password']:
-                return JsonResponse({"message": "INVALID_USER"}, status=401)
-                    
-            return JsonResponse({'message': "SUCCESS"}, status=200)
-        
+  
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
