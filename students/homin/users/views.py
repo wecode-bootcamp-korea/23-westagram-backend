c34743b95b37adfedaaa0d274d772e125a021f87
shm <thsrns3934@gmail.com>
2021-07-26 11:57:20 +0900

    Add: signup

--- students/homin/users/views.py
+++ students/homin/users/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+
+class ValidationError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+
+
+class UserView(View):
+    class ValidationError(Exception):
+        def __init__(self, msg):
+            self.msg = msg
+        def __str__(self):
+            return self.msg
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not re.match('^\w+@\w+\.\l+$', data['email']):
+                raise ValidationError('INVALID_EMAIL_FORMAT')
+
+            if (not re.match('\S{1:8}', data['password']) or
+                    len(data['password']) < 8):
+                raise ValidationError('INVALID_PASSWORD_FORMAT')
+
+            if not re.match('\d{3}-\d{3,4}-\d{4}', data['phone_number']):
+                raise ValidationError('INVALID_PHONE_NUMBER_FORMAT')
+
+            if User.objects.filter(email=data['email']).exist():
+                raise ValidationError('EXISTED_EMAIL')
+
+        except ValidationError as e:
+            return JsonResponse({"message": e}, status=400)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        
+#        except Exception:
+#            return JsonResponse({"message": "INVALID_VALUE"}, status=400)
+        
+        else:
+            user.objects.create(
+                name=data['name'],
+                email=data['email'],
+                password=data['password'],
+                phone_number=data['phone_number']
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+
+
+
+
+
