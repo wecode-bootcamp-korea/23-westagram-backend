6a0e902d9177e54616c297c9e207d31cb1e24752
Samdaso-o <soc05214@naver.com>
2021-07-26 20:05:50 +0900

    project_mission6_2

--- students/seunghun/users/utils.py
+++ students/seunghun/users/utils.py
@@ -9,7 +9,7 @@ def Keep_signup(func):
     def wrap(self,request):
         receive_token = request.header.get('Authorization')
         receive_id = jwt.decode(receive_token, SECRET_KEY, algorithms='HS256')
-        if receive_id == User.objects.get(id):
+        if receive_id == User.objects.get(id).exist():
             return JsonResponse({'MESSAGE':'VALID_TOKEN'}, status=200)
         return JsonResponse({'MESSAGE':'INVALID_TOKEN'}, status=400)
     return wrap
