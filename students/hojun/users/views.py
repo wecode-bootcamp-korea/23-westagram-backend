1e477a31a6a4e485e951c88d8c602cba75946b58
jun <wkdghwns91@gmail.com>
2021-07-26 17:31:48 +0900

    .

--- students/hojun/users/views.py
+++ students/hojun/users/views.py
@@ -1,5 +1,5 @@
 import json
-import re
+import re, bcrypt
 
 from django.http import JsonResponse
 from django.views import View
@@ -9,12 +9,13 @@ from users.models import User
 class UserView(View):
     def post(self, request):
         try:
-            data         = json.loads(request.body)
-            name         = data['name']
-            email        = data['email']
-            password     = data['password']
-            phone_number = data['phone_number']
-            age          = data['age']
+            data            = json.loads(request.body)
+            name            = data['name']
+            email           = data['email']
+            password        = data['password']
+            hashed_password = bcrypt.hashpw( password.encode('utf-8'), bcrypt.gensalt())
+            phone_number    = data['phone_number']
+            age             = data['age']
 
             password_validation = re.compile('\s')
             email_validation = re.compile('[\w]+@[\w]+[.]+[\w]+')
@@ -27,7 +28,7 @@ class UserView(View):
             User.objects.create(
                 name         = name,
                 email        = email,
-                password     = password,
+                password     = hashed_password,
                 phone_number = phone_number,
                 age          = age
             )
@@ -43,11 +44,12 @@ class LogInView(View):
             data = json.loads(request.body)
             email = data['email']
             password = data['password']
+            hashed_password = User.objects.get(email=email).password
 
             if not User.objects.filter(email=email).exists():
                 return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
             
-            if User.objects.get(email=email).password != password:
+            if not bcrypt.checkpw( password.encode('utf-8'), hashed_password.decode('utf-8')):
                 return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
 
             return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
